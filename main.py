4764575adcde755c991f51df3cc979fcc8cb3d2a
Tyler Hargrove <ts.hargrove95@gmail.com>
2022-12-12 14:29:17 -0600

    Working on "adding wines" functionality

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 import tkinter as tk
 import sqlite3
 from sqlite3 import Error
-
-
-def create_connection():
-    connection = None
-    try:
-        connection = sqlite3.connect(':memory:')
-        print("Connection to SQLite successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
+from wines import Wines
 
 FONT = ('Futura', 18, 'normal')
 
+connection = sqlite3.connect(':memory:')
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE wine (
+                name text,
+                grape text,
+                region text,
+                glass_price integer,
+                bottle_price integer
+                 )""")
 
-def add_wine():
+
+def add_wine_screen():
     window.minsize(width=750, height=600)
 
     wine_search_button.destroy()
@@ -38,24 +38,65 @@ def add_wine():
     wine_grape_entry = Entry(width=80)
     wine_grape_entry.grid(column=1, row=2, padx=3, pady=3)
 
+    wine_add_region = Label(text='Region:', font=FONT, fg='WHITE')
+    wine_add_region.grid(row=3, column=0, padx=3, pady=3)
+    wine_add_region.config(background='MAROON')
+    wine_region_entry = Entry(width=80)
+    wine_region_entry.grid(column=1, row=3, padx=3, pady=3)
+
     wine_add_glass_price = Label(text='Glass Price:', font=FONT, fg='WHITE')
-    wine_add_glass_price.grid(row=3, column=0, padx=3, pady=3)
+    wine_add_glass_price.grid(row=4, column=0, padx=3, pady=3)
     wine_add_glass_price.config(background='MAROON')
     wine_glass_entry = Entry(width=80)
-    wine_glass_entry.grid(column=1, row=3, padx=3, pady=3)
+    wine_glass_entry.grid(column=1, row=4, padx=3, pady=3)
 
     wine_add_bottle_price = Label(text='Bottle Price', font=FONT, fg='WHITE')
-    wine_add_bottle_price.grid(row=4, column=0, padx=3, pady=3)
+    wine_add_bottle_price.grid(row=5, column=0, padx=3, pady=3)
     wine_add_bottle_price.config(background='MAROON')
     wine_bottle_entry = Entry(width=80)
-    wine_bottle_entry.grid(column=1, row=4, padx=3, pady=3)
-
-    addition_button = Button(text="Add to Inventory!")
-    addition_button.grid(column=1, row=5)
-    addition_button.config(height=3, width=40)
+    wine_bottle_entry.grid(column=1, row=5, padx=3, pady=3)
 
     def add_to_database():
-        pass
+        try:
+            wine_name = wine_add_bar.get()
+            wine_grape = wine_grape_entry.get()
+            wine_region = wine_region_entry.get()
+            wine_glass_price = int(wine_glass_entry.get())
+            wine_bottle_price = int(wine_bottle_entry.get())
+            with connection:
+                cursor.execute("INSERT INTO wine VALUES (:name, :grape, :region, :glass_price, :bottle_price)",
+                               {
+                                   "name": wine_name, "grape": wine_grape, "region": wine_region,
+                                   "glass_price": wine_glass_price, "bottle_price": wine_bottle_price
+                               })
+
+            wine_add_name.destroy()
+            wine_add_bar.destroy()
+            wine_add_grape.destroy()
+            wine_grape_entry.destroy()
+            wine_add_region.destroy()
+            wine_region_entry.destroy()
+            wine_add_glass_price.destroy()
+            wine_glass_entry.destroy()
+            wine_add_bottle_price.destroy()
+            wine_bottle_entry.destroy()
+            addition_button.destroy()
+
+            home_wine_search_button = Button(text='Search', height=5, width=15, command=search_wine)
+            home_wine_search_button.grid(column=1, row=1, pady=2, padx=2)
+            home_wine_add_button = Button(text='Add', width=15, height=5, command=add_wine_screen)
+            home_wine_add_button.grid(column=0, row=1, pady=2)
+            home_wine_delete_button = Button(text='86', width=15, height=5)
+            home_wine_delete_button.grid(column=2, row=1, pady=2, padx=2)
+            window.minsize(width=750, height=400)
+
+        except ValueError:
+            messagebox.showinfo(title='Error', message="Make sure you've entered a numerical value for the glass"
+                                                       " and bottle prices")
+
+    addition_button = Button(text="Add to Inventory!", command=add_to_database)
+    addition_button.grid(column=1, row=6)
+    addition_button.config(height=3, width=40)
 
 
 def search_wine():
@@ -94,9 +135,6 @@ def search_wine():
     addition_button.grid(column=1, row=5)
     addition_button.config(height=3, width=40)
 
-    def add_to_database():
-        pass
-
 
 # Window Config ------------------------------------------------------
 window = tk.Tk()
@@ -111,31 +149,13 @@ canvas.create_image(250, 175, image=logo)
 canvas.grid(column=1, row=0)
 canvas.config(background='MAROON')
 
-# Wine Search Config ----------------------------------------------------
-# wine_search = Label(text='Search Wine:', font=FONT)
-# wine_search.grid(column=0, row=1)
-# wine_search.config(background='MAROON')
-# wine_search_entry = Entry(width=80)
-# wine_search_entry.focus()
-# wine_search_entry.grid(column=1, row=1, pady=2, padx=2)
+# Home Screen Buttons -------------------------------------------------
 wine_search_button = Button(text='Search', height=5, width=15, command=search_wine)
 wine_search_button.grid(column=1, row=1, pady=2, padx=2)
 
-# Wine Add Config -------------------------------------------------------
-# wine_add = Label(text='Add Wine:', font=FONT)
-# wine_add.grid(column=0, row=2)
-# wine_add.config(background='MAROON')
-# wine_add_entry = Entry(width=80)
-# wine_add_entry.grid(column=1, row=2)
-wine_add_button = Button(text='Add', width=15, height=5, command=add_wine)
+wine_add_button = Button(text='Add', width=15, height=5, command=add_wine_screen)
 wine_add_button.grid(column=0, row=1, pady=2)
 
-# Wine Delete Config -----------------------------------------------------
-# wine_delete = Label(text='86 a Wine:', font=FONT)
-# wine_delete.grid(column=0, row=3)
-# wine_delete.config(background='MAROON')
-# wine_delete_entry = Entry(width=80)
-# wine_delete_entry.grid(column=1, row=3)
 wine_delete_button = Button(text='86', width=15, height=5)
 wine_delete_button.grid(column=2, row=1, pady=2, padx=2)
 
--- wines.py
+++ wines.py
@@ -1,6 +1,8 @@
 class Wines:
-    def __init__(self, name, grape, glass_price):
+    def __init__(self, name, grape, region, glass_price, bottle_price):
         self.name = name
         self.grape = grape
+        self.region = region
         self.glass_price = glass_price
-        
+        self.bottle_price = bottle_price
+
